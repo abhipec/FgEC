1eb204df746a52e9cc29c4eb05f6bef68eb10d03
abhishek <abhishek.abhishek@iitg.ernet.in>
2018-12-08 19:15:29 +0530

    Added missing lib files.

---
+++ lib/compile_gcc_4.bash
@@ -0,0 +1,6 @@
+TF_INC=$(python3 -c 'import tensorflow as tf; print(tf.sysconfig.get_include())')
+TF_LIB=$(python -c 'import tensorflow as tf; print(tf.sysconfig.get_lib())')
+for i in *.cc; do
+  echo $i
+  g++ -std=c++11 -shared $i -o ${i::-2}so -fPIC -I $TF_INC -L$TF_LIB -ltensorflow_framework
+done
